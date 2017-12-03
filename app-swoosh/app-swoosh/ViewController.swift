8215068aef99d9e76a4ba0970caf8350d7521f2d
Hüseyin <kutbayh@gmail.com>
2017-12-03 14:40:51 +0300

    Transition between layout 1&2 complete

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -22,12 +22,9 @@ class ViewController: UIViewController {
 //        bgimg.frame = view.frame;
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+        
     }
-
-
+    
 }
 
